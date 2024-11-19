ceadfc7381bb060c2171a19bfb95a080e2195865
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-11-19 19:10:56 +0200

    add product controller

--- backend-app/src/main/java/mouse/univ/backendapp/api/ApiResponse.java
+++ backend-app/src/main/java/mouse/univ/backendapp/api/ApiResponse.java
@@ -2,6 +2,7 @@ package mouse.univ.backendapp.api;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import mouse.univ.backendapp.dto.product.ProductResponseDTO;
 
 @Data
 @NoArgsConstructor
@@ -13,5 +14,9 @@ public class ApiResponse<T> {
         this.success = success;
         this.error = error;
         this.data = data;
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(true, "", data);
     }
 }
--- backend-app/src/main/java/mouse/univ/backendapp/controller/ProductController.java
+++ backend-app/src/main/java/mouse/univ/backendapp/controller/ProductController.java
@@ -1,7 +1,13 @@
 package mouse.univ.backendapp.controller;
 
 import lombok.AllArgsConstructor;
+import mouse.univ.backendapp.api.ApiResponse;
+import mouse.univ.backendapp.dto.product.ProductCreateDTO;
+import mouse.univ.backendapp.dto.product.ProductResponseDTO;
+import mouse.univ.backendapp.dto.product.ProductUpdateDTO;
 import mouse.univ.backendapp.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,5 +18,31 @@ import java.util.List;
 public class ProductController {
     private final ProductService productService;
 
+    @PostMapping
+    public ResponseEntity<ApiResponse<ProductResponseDTO>> postProduct(@RequestBody ProductCreateDTO productCreateDTO) {
+        ProductResponseDTO result = productService.createProduct(productCreateDTO);
+        ApiResponse<ProductResponseDTO> apiResponse = ApiResponse.ok(result);
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
+
+    @PutMapping
+    public ResponseEntity<ApiResponse<ProductResponseDTO>> updateProduct(@RequestBody ProductUpdateDTO updateDTO) {
+        ProductResponseDTO result = productService.updateProduct(updateDTO);
+        ApiResponse<ProductResponseDTO> apiResponse = ApiResponse.ok(result);
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+    }
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<ProductResponseDTO>>> getAllProducts() {
+        List<ProductResponseDTO> result = productService.getAllProducts();
+        ApiResponse<List<ProductResponseDTO>> apiResponse = ApiResponse.ok(result);
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+    }
+
+    @GetMapping("/[id]")
+    public ResponseEntity<ApiResponse<ProductResponseDTO>> getAllProducts(@RequestParam Long id) {
+        ProductResponseDTO result = productService.findProductById(id);
+        ApiResponse<ProductResponseDTO> apiResponse = ApiResponse.ok(result);
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+    }
 
 }
